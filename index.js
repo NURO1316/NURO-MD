c2c975ee143062d4632d23491fde41f28e4133e0
NURO1316 <tharakadilshan035@gmail.com>
2025-02-25 21:20:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -77,23 +77,33 @@ console.log("Session downloaded ✅")
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 9090;
-  
-  //=============================================
-  
-  async function connectToWA() {
-  console.log("Connecting to WhatsApp ⏳️...");
-  const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/sessions/')
-  var { version } = await fetchLatestBaileysVersion()
-  
-  const conn = makeWASocket({
-          logger: P({ level: 'silent' }),
-          printQRInTerminal: false,
-          browser: Browsers.macOS("Firefox"),
-          syncFullHistory: true,
-          auth: state,
-          version
-          })
+const port = process.env.PORT || 8000;
+async function connectToWA() {
+    const connectDB = require('./lib/mongodb');
+    connectDB();
+    const {
+        readEnv
+    } = require('./lib/database');
+    const config = await readEnv();
+    const prefix = config.PREFIX;
+    console.log("Connecting wa bot 🧬...");
+    const {
+        state,
+        saveCreds
+    } = await useMultiFileAuthState(__dirname + '/sessions/');
+    var {
+        version
+    } = await fetchLatestBaileysVersion();
+    const conn = makeWASocket({
+        logger: P({
+            level: 'silent'
+        }),
+        printQRInTerminal: false,
+        browser: Browsers.macOS("Firefox"),
+        syncFullHistory: true,
+        auth: state,
+        version
+    })
       
   conn.ev.on('connection.update', (update) => {
   const { connection, lastDisconnect } = update
@@ -145,7 +155,7 @@ const port = process.env.PORT || 9090;
   }
     if(mek.message.viewOnceMessageV2)
     mek.message = (getContentType(mek.message) === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
-    if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_STATUS_SEEN === "true"){
+    if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_READ_STATUS === "true"){
       await conn.readMessages([mek.key])
     }
   if (mek.key && mek.key.remoteJid === 'status@broadcast' && config.AUTO_STATUS_REACT === "true"){
